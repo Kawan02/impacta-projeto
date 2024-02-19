7bf45868cbcf7f142ef34126c1db3e2dd452275d
Kawan02 <kawanng39@gmail.com>
2024-02-18 21:12:53 -0300

    ajustes

---
+++ Front/app_contatos/lib/src/services/dependency_injection.dart
@@ -0,0 +1,10 @@
+import 'package:app_contatos/src/services/network.dart';
+import 'package:app_contatos/src/views/home/controllers/home_controller.dart';
+import 'package:get/get.dart';
+
+class DependecyInjection {
+  static void init() {
+    Get.put<NetworkController>(NetworkController(), permanent: true);
+    Get.put(HomeController());
+  }
+}
