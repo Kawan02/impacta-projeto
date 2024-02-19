42d6f5ebd60fae11e306833684f07a43fe9332b5
Kawan02 <kawanng39@gmail.com>
2024-02-19 12:57:54 -0300

    Software para o "tcc" em andamento

--- Front/app_contatos/lib/src/services/dependency_injection.dart
+++ Front/app_contatos/lib/src/services/dependency_injection.dart
@@ -1,9 +1,17 @@
 import 'package:app_contatos/src/services/network.dart';
+import 'package:app_contatos/src/views/home/controllers/contatos_controller.dart';
 import 'package:get/get.dart';
 
 class DependecyInjection {
   static void init() {
     Get.put<NetworkController>(NetworkController(), permanent: true);
-    // Get.put(HomeController());
+    Get.lazyPut<ContatosController>(() => ContatosController());
   }
 }
+
+// class SampleBind extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut<ContatosController>(() => ContatosController());
+//   }
+// }
