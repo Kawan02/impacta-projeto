985607e9cee267c30e98c20a981029d69bd98d78
Kawan02 <kawanng39@gmail.com>
2024-03-02 17:22:12 -0300

    Removendo pasta do projeto antigo criado errado

--- Front/app_contatos/lib/src/routes/api_routes.dart
+++
@@ -1,6 +0,0 @@
-class ApiRoutes {
-  static String baseUrl = "http://localhost:8080";
-  static String getContatos = "$baseUrl/contacts";
-  static String postContatos = "$baseUrl/contacts";
-  static String putContato(int id) => "$baseUrl/contact/$id";
-}
